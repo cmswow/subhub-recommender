e82491a874a1791cec9f20469316401bde7ea31c
cmswow <127907534+cmswow@users.noreply.github.com>
2024-03-17 18:12:18 +0900

    Initial commit

---
+++ src/main/java/com/subhub/subscriptionmanager/SubHubConnectApplication.java
@@ -0,0 +1,13 @@
+package com.subhub.subscriptionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubHubConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubHubConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subhub/subscriptionmanager/SubHubConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhub.subscriptionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubHubConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
